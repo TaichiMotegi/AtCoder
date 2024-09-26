7c277da4ca848eae206cfd1b01083998d27320cf
t-motegi <motegi.web@gmail.com>
2024-09-26 14:45:05 +0000

    update：abc231-a

--- abc231/a/main.py
+++ abc231/a/main.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-S = input()
-N = int(input())
-S = input().split()
-A, B, C = input().split()
-L = list(map(int, input().split()))
-H, N = map(int, input().split())
+
+D = int(input())
+
+print(D / 100)

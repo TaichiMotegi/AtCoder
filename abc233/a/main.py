23854ab01ae70c8cb484e942ead7e110b206d3f7
t-motegi <motegi.web@gmail.com>
2024-10-14 15:00:03 +0000

    ✨ add：abc236

--- abc233/a/main.py
+++ abc233/a/main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-S = input()
-N = int(input())
-S = input().split()
-A, B, C = input().split()
-L = list(map(int, input().split()))
+
 H, N = map(int, input().split())
+
+if H > N:
+    print(0)
+else:
+    wh

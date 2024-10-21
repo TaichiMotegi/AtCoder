fe8d3a4f731f401e61a544131ed66c974fe91b2b
t-motegi <motegi.web@gmail.com>
2024-10-21 14:58:43 +0000

    🔨 update：abc233-b

--- abc233/b/main.py
+++ abc233/b/main.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-import math
+L, R = map(int, input().split())
+S = input()
 
-N = int(input())
-dot = [list(map(int, input().split())) for _ in range(N)]
-ans = 0
+hoge = []
+count = 0
+for i in range(len(S)):
+    if L <= i + 1 <= R:
+        hoge.append(S[R - 1 - count])
+        count += 1
+    else:
+        hoge.append(S[i])
 
-for i in range(N):
-    for j in range(i + 1, N):
-        if ans < math.sqrt((i[0] - j[0]) ** 2 + (i[1] - j[1]) ** 2):
-            ans = math.sqrt((i[0] - j[0]) ** 2 + (i[1] - j[1]) ** 2)
-print(ans)
+print("".join(hoge))

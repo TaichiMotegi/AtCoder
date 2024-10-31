e5f9e07b6847e2dcb59279ad8a71a29b86e3bba6
t-motegi <motegi.web@gmail.com>
2024-10-31 14:52:41 +0000

    🔨 update：abc235-a

--- abc235/a/main.py
+++ abc235/a/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-S = input()
-N = int(input())
-S = input().split()
-A, B, C = input().split()
-L = list(map(int, input().split()))
-H, N = map(int, input().split())
+N = input()
+a = N[0]
+b = N[1]
+c = N[2]
+ans = int(a + b + c) + int(b + c + a) + int(c + a + b)
+print(ans)
